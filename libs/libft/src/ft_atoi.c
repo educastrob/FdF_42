0e355c409a16b442aab3ed54c4705047eeb51bc8
Eduardo Castro <eduardocastrobritoo@gmail.com>
2023-11-06 11:54:00 -0300

    .

---
+++ libs/libft/src/ft_atoi.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwallage <mwallage@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 16:33:11 by mwallage          #+#    #+#             */
+/*   Updated: 2023/09/21 16:30:40 by mwallage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	neg;
+	int	result;
+
+	while (*nptr == ' ' || *nptr == '\t' || *nptr == '\n' || *nptr == '\v'
+		|| *nptr == '\f' || *nptr == '\r')
+		nptr++;
+	neg = 1;
+	if (*nptr == '-')
+	{
+		neg = -1;
+		nptr++;
+	}
+	else if (*nptr == '+')
+		nptr++;
+	result = 0;
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		result = result * 10 + *nptr - '0';
+		nptr++;
+	}
+	return (neg * result);
+}
+
+int	ft_atoi_base(const char *nptr, const char *base)
+{
+	int	neg;
+	int	result;
+	int	nbase;	
+
+	while (*nptr == ' ' || *nptr == '\t' || *nptr == '\n' || *nptr == '\v'
+		|| *nptr == '\f' || *nptr == '\r')
+		nptr++;
+	neg = 1;
+	if (*nptr == '-')
+	{
+		neg = -1;
+		nptr++;
+	}
+	else if (*nptr == '+')
+		nptr++;
+	nbase = ft_strlen(base);
+	result = 0;
+	while (nptr && *nptr && ft_strchr(base, *nptr))
+	{
+		result = result * nbase + ft_strchr(base, *nptr) - base;
+		nptr++;
+	}
+	return (neg * result);
+}
